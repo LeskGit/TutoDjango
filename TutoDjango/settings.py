b1adf69e6393bd37ee88f92cd217a8085eee87c8
LeskGit <hacksel002@gmail.com>
2025-09-08 16:08:05 +0200

    toolbar

--- TutoDjango/settings.py
+++ TutoDjango/settings.py
@@ -39,6 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Mes apps
     'monApp',
+    'debug_toolbar',
+]
+
+INTERNAL_IPS = [
+# ...
+"127.0.0.1",
+# ...
 ]
 
 MIDDLEWARE = [
@@ -49,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'TutoDjango.urls'
