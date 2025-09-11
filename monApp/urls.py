b95d9d4f6adba99f4bf53896e2e2c6997038f75b
LeskGit <hacksel002@gmail.com>
2025-09-11 10:56:52 +0200

    HomeView et AboutView créer et fonctionnel

--- monApp/urls.py
+++ monApp/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
+from django.views.generic import *
 
 urlpatterns = [
-    path('home',views.home ,name='home'), # Par defaut 
-    path('home/<str:param>',views.home ,name='home'),
+    #path('home',views.home ,name='home'), # Par defaut 
+    #path('home/<str:param>',views.home ,name='home'),
+    path("home", views.HomeView.as_view()),
     path("contact", views.contact, name="contact"),
-    path("about", views.about, name="about"),
+    path("about", views.AboutView.as_view()),
+    #path("about", views.about, name="about"),
     path("products", views.listProduits, name="products"),
     path("categories", views.listCategories, name="categories"),
     path("rayons", views.listRayon, name="rayons"),
--- monApp/views.py
+++ monApp/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from monApp.models import Produit, Categorie, Status, Rayon
+from django.views.generic import *
+
+
+class HomeView(TemplateView):
+    template_name = "monApp/page_home.html"
+    
+    def post(self, request, **kwargs):
+        return render(request, self.template_name)
+
+class AboutView(TemplateView):
+    template_name = "monApp/about.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super(AboutView, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
+        context["titreh1"] = "About us..."
+        return context
+    
+    def post(self, request, **kwargs):
+        return render(request, self.template_name)
+        
+
 
 # Create your views here.
 def home(request, param="Coubeh"):
