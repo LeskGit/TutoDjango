ab5e50e6bb6e27aecd81fa856d87a6dff17de705
LeskGit <hacksel002@gmail.com>
2025-09-15 16:49:46 +0200

    Vue génerique (je passe à la connexion déconnexion)

--- monApp/views.py
+++ monApp/views.py
@@ -9,19 +9,106 @@ class HomeView(TemplateView):
     
     def post(self, request, **kwargs):
         return render(request, self.template_name)
-
-class AboutView(TemplateView):
-    template_name = "monApp/about.html"
     
     def get_context_data(self, **kwargs):
+        param = self.kwargs.get('param')
+        if param == None:
+            param = "XiaoPang"
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['titreh1'] = param
+        return context
+
+
+class AboutView(TemplateView):
+    template_name = "monApp/page_home.html"
+    
+    def get_context_data(self, **kwargs):
+        param = self.kwargs.get('param')
+        if param == None:
+            param = "XiaoPang"
         context = super(AboutView, self).get_context_data(**kwargs)
-        context = super().get_context_data(**kwargs)
-        context["titreh1"] = "About us..."
+        context["titreh1"] = param
+        context["desc"] = "Axel Meunier, BUT 3, Loup"
         return context
     
     def post(self, request, **kwargs):
         return render(request, self.template_name)
+
+class ContactView(TemplateView):
+    template_name = "monApp/page_home.html"
+    
+    def get_context_data(self, **kwargs):
+        param = self.kwargs.get('param')
+        if param == None:
+            param = "XiaoPang"
+        context = super(ContactView, self).get_context_data(**kwargs)
+        context["titreh1"] = param
+        context["desc"] = "axel.meunier49@gmail.com"
+        return context
+    
+    def post(self, request, **kwargs):
+        return render(request, self.template_name)
+    
+class ProduitListView(ListView):
+    model = Produit
+    template_name = "monApp/list_produits.html"
+    context_object_name = "prdts"
+    # queryset = Produit.objects.filter(id=2)
+    
+    def get_queryset(self ) :
+        return Produit.objects.order_by("prixUnitaireProd")
+    
+    def get_context_data(self, **kwargs):
+        context = super(ProduitListView, self).get_context_data(**kwargs)
+        context['titremenu'] = "Liste de mes produits"
+        return context
+
+
+class ProduitDetailView(DetailView):
+    model = Produit
+    template_name = "monApp/detail_produit.html"
+    context_object_name = "prdt"
+    
+    def get_context_data(self, **kwargs):
+        context = super(ProduitDetailView, self).get_context_data(**kwargs)
+        context['titremenu'] = "Détail du produit"
+        return context
         
+
+
+class CategorieDetailView(DetailView):
+    model = Categorie
+    template_name = "monApp/detail_categorie.html"
+    context_object_name = "ctds"
+    
+    def get_context_data(self, **kwargs):
+        context = super(CategorieDetailView, self).get_context_data(**kwargs)
+        context['titremenu'] = "Détail de la catégorie"
+        return context
+        
+
+
+class StatusDetailView(DetailView):
+    model = Status
+    template_name = "monApp/detail_status.html"
+    context_object_name = "status"
+    
+    def get_context_data(self, **kwargs):
+        context = super(StatusDetailView, self).get_context_data(**kwargs)
+        context['titremenu'] = "Détail du status"
+        return context
+        
+class RayonDetailView(DetailView):
+    model = Rayon
+    template_name = "monApp/detail_rayon.html"
+    context_object_name = "rayons"
+    
+    def get_context_data(self, **kwargs):
+        context = super(RayonDetailView, self).get_context_data(**kwargs)
+        context['titremenu'] = "Détail du rayon"
+        return context
+        
+
 
 
 # Create your views here.
